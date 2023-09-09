b5f4a907a611d8d13587b36232a7c3429f8ac0a7
JHTNT <jerry1106666@gmail.com>
2023-09-09 23:16:29 +0800

    feat: add file and directory to queue

--- app/components/widgets/file_item.py
+++ app/components/widgets/file_item.py
@@ -11,17 +11,25 @@ from PySide6.QtWidgets import (
 )
 
 
-class CustomQWidget(QWidget):
-    def __init__(self, path: str):
+class FileItem(QWidget):
+    def __init__(self, path: str, is_directory: bool = False):
         super().__init__()
 
         self.file_path = QLabel(path, self)
+
         self.spacer = QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
+
         self.batch_progress = QProgressBar(self)
+        self.batch_progress.setFormat("%v/%m")
         self.batch_progress.setValue(0)
-        self.batch_progress.setMaximum(self.count_file(path))
+        if is_directory:
+            self.file_cnt = self.count_file(path)
+        else:
+            self.file_cnt = 1
+        self.batch_progress.setMaximum(self.file_cnt)
         self.batch_progress.setSizePolicy(QSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed))
-        self.status = QLabel("成功", self)
+
+        self.status = QLabel("等待壓縮", self)
 
         self.horizontal_layout = QHBoxLayout()
         self.horizontal_layout.setSizeConstraint(QLayout.SetDefaultConstraint)
@@ -29,14 +37,14 @@ class CustomQWidget(QWidget):
         self.horizontal_layout.addItem(self.spacer)
         self.horizontal_layout.addWidget(self.batch_progress)
         self.horizontal_layout.addWidget(self.status)
+        # self.horizontal_layout.addWidget(self.remove_button)
         self.setLayout(self.horizontal_layout)
 
     def count_file(self, path: str):
-        if os.path.isfile(path):
-            return 1
-        else:
-            count = 0
-            for item in os.listdir(path):
-                if os.path.isfile(path + item):
-                    count += 1
-            return count
+        count = 0
+        for item in os.listdir(path):
+            if os.path.isfile(path + "/" + item) and item.endswith(
+                (".png", ".jpg", ".jpeg", ".tiff", ".webp")
+            ):
+                count += 1
+        return count
--- app/main.py
+++ app/main.py
@@ -1,14 +1,18 @@
 import sys
 
+from components.widgets.file_item import FileItem
 from PySide6.QtGui import QAction, QIcon
 from PySide6.QtWidgets import (
     QApplication,
+    QFileDialog,
     QHBoxLayout,
     QListWidget,
+    QListWidgetItem,
     QMainWindow,
     QMenu,
     QMenuBar,
     QPushButton,
+    QStyle,
     QVBoxLayout,
     QWidget,
 )
@@ -26,7 +30,7 @@ class MainWindow(QMainWindow):
         self.queueing_files_list = QListWidget(self)
 
         self.add_file_action = QAction("添加檔案", self)
-        self.add_folder_action = QAction("添加資料夾", self)
+        self.add_directory_action = QAction("添加資料夾", self)
 
         self.file_menu = QMenu("檔案", self)
         self.about_menu = QMenu("關於", self)
@@ -38,11 +42,14 @@ class MainWindow(QMainWindow):
         # layout of the buttons at the bottom
         self.function_bar_layout = QHBoxLayout()
         self.init_widget()
+        self.init_layout()
         self.init_window()
 
     def init_widget(self):
         self.setCentralWidget(self.main_widget)
-        self.init_layout()
+
+        self.add_file_action.triggered.connect(self.add_file)
+        self.add_directory_action.triggered.connect(self.add_directory)
 
     def init_layout(self):
         self.main_layout.addWidget(self.queueing_files_list)
@@ -56,13 +63,33 @@ class MainWindow(QMainWindow):
 
     def init_window(self):
         self.file_menu.addAction(self.add_file_action)
-        self.file_menu.addAction(self.add_folder_action)
+        self.file_menu.addAction(self.add_directory_action)
         self.menubar.addAction(self.file_menu.menuAction())
         self.menubar.addAction(self.about_menu.menuAction())
         self.setMenuBar(self.menubar)
         self.setWindowTitle("WebP GUI tool")
         self.setWindowIcon(QIcon(".\\app\\resources\\logo.ico"))
-        # self.resize(991, 656)
+
+    def add_file(self):
+        filename = QFileDialog.getOpenFileNames(
+            self, "選擇檔案", filter="圖片 (*.png *.jpg *.jpeg *.tiff *.webp)"
+        )
+        for file in filename[0]:
+            item = QListWidgetItem()
+            widget = FileItem(file)
+            item.setIcon(self.style().standardIcon(QStyle.SP_FileIcon))
+            item.setSizeHint(widget.sizeHint())
+            self.queueing_files_list.addItem(item)
+            self.queueing_files_list.setItemWidget(item, widget)
+
+    def add_directory(self):
+        dirname = QFileDialog.getExistingDirectory(self, "選擇資料夾")
+        item = QListWidgetItem()
+        widget = FileItem(dirname, True)
+        item.setIcon(self.style().standardIcon(QStyle.SP_DirIcon))
+        item.setSizeHint(widget.sizeHint())
+        self.queueing_files_list.addItem(item)
+        self.queueing_files_list.setItemWidget(item, widget)
 
 
 if __name__ == "__main__":
